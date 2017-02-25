e1531dc3dcbe89c0581894e0c3ea0b37ae4f147d
unknown <>
2017-02-25 17:38:39 +0900

    課題：自己紹介を提出します。

---
+++ CampChallenge_PHP/CampChallenge_PHP/001.プログラミング基礎学習１/003programming_1.php
@@ -0,0 +1,20 @@
+<?php
+$firstname = 'Shoko';
+$lastname = 'Meguro';
+$hometown = 'Sendai';
+$hobby = 'Reading books';
+function br(){ echo nl2br("\n");}
+
+echo 'Hello, guys!';
+br();
+echo 'I would like to introduce myself briefly.';
+br();
+echo 'I am '.$firstname.$lastname.'.';
+br();
+echo 'I was born in:'.$hometown.'.';
+br();
+echo 'My favorite hobby is'.$hobby.'.';
+br();
+echo 'I love Japanese authors in Meji era in particular'.'.';
+br();
+echo 'Thank you for listening!';
