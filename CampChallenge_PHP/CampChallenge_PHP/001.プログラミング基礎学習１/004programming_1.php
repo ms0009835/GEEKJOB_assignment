3e67742b40da660ce76f0c161bb076a9865421d3
unknown <>
2017-02-25 18:00:20 +0900

    課題：四則演算を提出します。

---
+++ CampChallenge_PHP/CampChallenge_PHP/001.プログラミング基礎学習１/004programming_1.php
@@ -0,0 +1,17 @@
+<?php
+$unstable = 10;
+const stable = 2;
+
+function br(){ echo nl2br("\n");}
+
+//additon
+echo 'Addition: '.($unstable+stable);
+br();
+//subtraction
+echo 'Subtraction: '.($unstable-stable);
+br();
+//multiplication
+echo 'Multiphication: '.($unstable*stable);
+br();
+//division
+echo 'Division: '.($unstable/stable);
