241fde44a097164f4bd389e1910406fbc6a091f6
unknown <>
2017-02-27 17:40:38 +0900

    課題：クエリストリングの利用を提出します

--- CampChallenge_PHP/CampChallenge_PHP/001.プログラミング基礎学習１/006programming_1.php
+++ CampChallenge_PHP/CampChallenge_PHP/001.プログラミング基礎学習１/006programming_1.php
@@ -1,38 +1,42 @@
 <?php
-function br(){ echo nl2br("\n");}
+function br(){echo nl2br("\n");}
+function calculate($total,$number){
+  $prise=$total/$number;
+  br();
+  echo '1個当たり'.$prise.'円です。';
+  return $prise;
+}
 
 $item=0;
 const item1='１：雑貨';
 const item2='２：生鮮食品';
 const item3='３：その他';
 
+$item=$_GET['item'];
 $total=$_GET['total'];
 $number=$_GET['number'];
 $prise=0;
-
 $point=0;
 
 //start
-echo '商品種別';
+echo '商品種別 ';
 echo item1.'/'.item2.'/'.item3;
-
+br();
 if ($item ==1){
-    echo '購入商品：'.item1;
-    br();
-    $prise=$total/$number;
-    echo '1個当たり'.$prise.'です。';
-  }elseif($item == 2){
-    echo '購入商品：'.item2;
-    br();
-    $prise=$total/$number;
-    echo '1個当たり'.$prise.'です。';}else{echo '購入商品：'.item3;
-    br();
-    $prise=$total/$number;
-    echo '1,個当たり'.$prise.'円です。';}
-
+    echo '購入商品 '.item1;
+    calculate($total,$number);
+    }elseif($item == 2){
+    echo '購入商品 '.item2;
+    calculate($total,$number);
+  }elseif($item==3){
+    echo '購入商品 '.item3;
+    calculate($total,$number);
+    }
+br();
     if(3000<=$total){
       $point= $total*0.04;
       echo $point.'ポイント獲得しました。';
     }elseif(5000<=$total){
       $point= $total*0.05;
+      echo $point.'ポイント獲得しました。';
     }
