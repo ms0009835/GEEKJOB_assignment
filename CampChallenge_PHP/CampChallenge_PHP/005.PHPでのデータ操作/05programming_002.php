6162ae3922eba04e70540e82356fc728cb658297
unknown <>
2017-03-14 15:56:08 +0900

    クッキーの記録と表示を提出します。

---
+++ CampChallenge_PHP/CampChallenge_PHP/005.PHPでのデータ操作/05programming_002.php
@@ -0,0 +1,14 @@
+<?PHP
+
+/*
+クッキーに現在時刻を記録し、
+次にアクセスした際に、前回記録した日時を表示してください。
+*/
+date_default_timezone_set('Asia/Tokyo');
+
+$access_time=date('Y/m/d H:i:s');
+setcookie('LastLoginDate',$access_time);
+
+$lastDate = $_COOKIE['LastLoginDate'];
+
+echo '前回記録した日時は、'.$lastDate.'です';
